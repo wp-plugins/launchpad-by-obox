0e5b1d9c74f4968c482b07f2f62fb1f5a3eec2a1
obox <obox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-27 08:21:53 +0000

    Layers link update
    git-svn-id: https://plugins.svn.wordpress.org/launchpad-by-obox/tags/1.0.7@1168315 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/*
+	Plugin Name: Launchpad by Obox
+	Plugin URI: http://oboxthemes.com
+	Description: Deploys a beautiful, responsive Coming Soon or Down for Maintenance page for your site. Collect emails, change styles, show social links and more
+	Author: Obox Design
+	Version: 1.0.7
+	Author URI: http://oboxthemes.com
+*/
+
+define("LAUNCHPADDIR",  plugin_dir_path( __FILE__ ) );
+
+// Kick off Plugin
+function apollo_includes(){
+	include_once ("functions/load-includes.php");
+	$launchpad = new apollo_launchpad();
+	$launchpad->initiate();
+}
+add_action("plugins_loaded", "apollo_includes");
+
+function apollo_admin(){
+	$settings = new apollo_launchpad_settings();
+	$settings->init();
+}
+add_action("plugins_loaded", "apollo_admin");
+
+function apollo_activate() {
+	add_option('apollo_do_activation_redirect', true);
+}
+
+function apollo_redirect() {
+	if (get_option('apollo_do_activation_redirect', false)) {
+		delete_option('apollo_do_activation_redirect');
+		wp_redirect(admin_url('admin.php?page=apollo_general_settings'));
+	}
+}
+register_activation_hook(__FILE__, 'apollo_activate');
+add_action('admin_init', 'apollo_redirect');
